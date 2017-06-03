9de22ba7d26f006d5e016c4e1e232c66600d2c41
Billly <lyy0808@yeah.net>
2017-06-03 23:34:31 +0800

    修改代码结构

---
+++ AdapterModel/EVLidarObjectAdapter.cs
@@ -0,0 +1,20 @@
+﻿namespace AdapterModel
+{
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   对象适配器. </summary>
+    ///
+    /// <remarks>   Billly, 2017:06:03 23:25:49. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
+    public class EVLidarObjectAdapter: A7TargetLidar
+    {
+        public LidarEV D3EvLidar = new LidarEV();
+
+        public override void Request()
+        {
+            D3EvLidar.SpecificRequest();
+        }
+
+
+    }
+}
--- AdapterModel/ObjectAdapter.cs
+++ AdapterModel/ObjectAdapter.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdapterModel
 {
-    class ObjectAdapter
-    {
-    }
+    
     public class A7TargetLidar
     {
         ///-------------------------------------------------------------------------------------------------
@@ -18,37 +15,5 @@ namespace AdapterModel
         ///-------------------------------------------------------------------------------------------------
 
         public virtual  void Request() { }
-    }
-
-    ///-------------------------------------------------------------------------------------------------
-    /// <summary>  要适配的类. </summary>
-    ///
-    /// <remarks>   Billly, 2017:06:03 23:25:27. </remarks>
-    ///-------------------------------------------------------------------------------------------------
-
-    public  class LidarEV
-    {
-        public void SpecificRequest()
-        {
-            Console.WriteLine("我是EV雷达");
-        }
-    }
-
-    ///-------------------------------------------------------------------------------------------------
-    /// <summary>   对象适配器. </summary>
-    ///
-    /// <remarks>   Billly, 2017:06:03 23:25:49. </remarks>
-    ///-------------------------------------------------------------------------------------------------
-
-    public class EVLidarObjectAdapter: A7TargetLidar
-    {
-        public LidarEV D3EvLidar = new LidarEV();
-
-        public override void Request()
-        {
-            D3EvLidar.SpecificRequest();
-        }
-
-
     }
 }
