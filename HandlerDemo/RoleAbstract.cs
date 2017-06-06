b8703fd6980608edeeaf56762bf23d739a4ae9e3
Billly <lyy0808@yeah.net>
2017-06-06 08:26:06 +0800

    添加注释

--- HandlerDemo/RoleAbstract.cs
+++ HandlerDemo/RoleAbstract.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿///=================================================================================================
+// file:	RoleAbstract.cs
+//
+// summary:	Implements the role abstract class
+///-------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +12,81 @@ using System.Threading.Tasks;
 
 namespace HandlerDemo
 {
+    ///=================================================================================================
+    /// <summary>   A role abstract. </summary>
+    ///
+    /// <remarks>   lyy, 2017:6:5_9:58:0. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
     public abstract class RoleAbstract
     {
+        ///=================================================================================================
+        /// <summary>   Gets or sets the next role. </summary>
+        ///
+        /// <value> The next role. </value>
+        ///-------------------------------------------------------------------------------------------------
+
         public RoleAbstract NextRole { get; set; }
 
+        ///=================================================================================================
+        /// <summary>   Gets or sets the name. </summary>
+        ///
+        /// <value> The name. </value>
+        ///-------------------------------------------------------------------------------------------------
+
         public string name { get; set; }
+
+        ///=================================================================================================
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:58:0. </remarks>
+        ///
+        /// <param name="strName">  The name. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public RoleAbstract(string strName)
         {
             name = strName;
         }
 
+        ///=================================================================================================
+        /// <summary>   Behaviours the given request. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:58:0. </remarks>
+        ///
+        /// <param name="request">  The request. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public abstract void Behaviour(BehaviourRequest request);
     }
+
+   ///=================================================================================================
+   /// <summary>    A manager role. </summary>
+   ///
+   /// <remarks>    lyy, 2017:6:5_9:57:59. </remarks>
+   ///-------------------------------------------------------------------------------------------------
+
    public class ManagerRole : RoleAbstract
     {
+        ///=================================================================================================
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="strName">  The name. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public ManagerRole(string strName) : base(strName)
         {
         }
+
+        ///=================================================================================================
+        /// <summary>   Behaviours the given request. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="request">  The request. </param>
+        ///-------------------------------------------------------------------------------------------------
 
         public override void Behaviour(BehaviourRequest request)
         {
@@ -38,11 +102,33 @@ namespace HandlerDemo
         }
     }
 
+    ///=================================================================================================
+    /// <summary>   A president role. </summary>
+    ///
+    /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
     public class PresidentRole : RoleAbstract
     {
+        ///=================================================================================================
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="strName">  The name. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public PresidentRole(string strName) : base(strName)
         {
         }
+
+        ///=================================================================================================
+        /// <summary>   Behaviours the given request. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="request">  The request. </param>
+        ///-------------------------------------------------------------------------------------------------
 
         public override void Behaviour(BehaviourRequest request)
         {
@@ -59,11 +145,33 @@ namespace HandlerDemo
         }
     }
 
+    ///=================================================================================================
+    /// <summary>   A presigengt role. </summary>
+    ///
+    /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
     public class PresigengtRole : RoleAbstract
     {
+        ///=================================================================================================
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="strName">  The name. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public PresigengtRole(string strName) : base(strName)
         {
         }
+
+        ///=================================================================================================
+        /// <summary>   Behaviours the given request. </summary>
+        ///
+        /// <remarks>   lyy, 2017:6:5_9:57:59. </remarks>
+        ///
+        /// <param name="request">  The request. </param>
+        ///-------------------------------------------------------------------------------------------------
 
         public override void Behaviour(BehaviourRequest request)
         {
